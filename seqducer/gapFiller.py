7e6cedd6c7388112a2de6dd847e2253b302ca1bb
ceilinglight <45287238+ceilinglight@users.noreply.github.com>
2020-11-27 02:23:30 +0700

    Update gapFiller.py

--- seqducer/gapFiller.py
+++ seqducer/gapFiller.py
@@ -4,39 +4,44 @@ from Bio import SeqIO
 from collections import defaultdict
 import sys
 
-reads = defaultdict(dict)
-# To do: Convert sys.argv to argparse
-# Open reads.fasta
-with open(sys.argv[1]) as in_file:
-    for record in SeqIO.parse(in_file, 'fasta'):
-        reads[records.id]['record'] = record
+def main():
+    reads = defaultdict(dict)
+    # To do: Convert sys.argv to argparse
+    # Open reads.fasta
+    with open(sys.argv[1]) as in_file:
+        for record in SeqIO.parse(in_file, 'fasta'):
+            reads[records.id]['record'] = record
 
-# Open reads.coor
-with open(sys.argv[2]) as in_file:
-    for line in in_file:
-        line = line.split()
-        if 'hit' in reads[line[0]].keys():
-            continue
-        reads[line[0]]['hit'] = line
+    # Open reads.coor
+    with open(sys.argv[2]) as in_file:
+        for line in in_file:
+            line = line.split()
+            if 'hit' in reads[line[0]].keys():
+                continue
+            reads[line[0]]['hit'] = line
 
-# Calculate offset value
-for identifier, value in reads.items():
-    if value['hit'][5] == 'plus':
-        offset = int(value['hit'][3])-int(value['hit'][1])
-    else:
-        offset = int(value['hit'][4])-int(value['hit'][1])
-    reads[identifier]['offset'] = offset
+    # Calculate offset value
+    for identifier, value in reads.items():
+        if value['hit'][5] == 'plus':
+            offset = int(value['hit'][3])-int(value['hit'][1])
+        else:
+            offset = int(value['hit'][4])-int(value['hit'][1])
+        reads[identifier]['offset'] = offset
 
-output_string = ''
+    output_string = ''
 
-# Justify offset
-min_offset = min([value['offset'] for value in reads.values()])
-for identifier, value in reads.items():
-    reads[identifier]['offset'] -= min_offset
-    output_string += f'>{identifier}\n'
-    output_string += '-'*reads[identifier]['offset']
-    output_string += str(value['record'].seq.upper())
-    output_string += '\n'
+    # Justify offset
+    min_offset = min([value['offset'] for value in reads.values()])
+    for identifier, value in reads.items():
+        reads[identifier]['offset'] -= min_offset
+        output_string += f'>{identifier}\n'
+        output_string += '-'*reads[identifier]['offset']
+        output_string += str(value['record'].seq.upper())
+        output_string += '\n'
 
-with open(sys.arg[3], 'w') as out_file:
-    out_file.write(output_string)
+    # with open(sys.arg[3], 'w') as out_file:
+    #     out_file.write(output_string)
+    print(output_string)
+
+if __name__ == "__main__":
+    main()
